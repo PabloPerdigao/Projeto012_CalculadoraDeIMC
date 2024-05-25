53d21b2f9be333879b350b5a7c84c1804c6aa962
Pablo Perdigão <pabloagps@gmail.com>
2024-05-25 15:53:34 -0300

    add "open" para trocar a mensagem do modal

--- script.js
+++ script.js
@@ -4,6 +4,15 @@
   const inputWeight = document.querySelector('#weight')
   const inputHeight = document.querySelector('#height')
 
+  const modalWrapper = document.querySelector('.modal-wrapper')
+  const modalMessage = document.querySelector('.modal .title span')
+  const modalbtnClose = document.querySelector('.modal button.close')
+
+// abrir o modal
+  modalMessage.innertext = "anything"
+// add "open" para trocar a mensagem do modal
+  modalWrapper.classList.add('open')
+
 /* 3 maneiras de criar e atribuir função a um evento */
   
 /*1ª forma:
