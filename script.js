7afca5d907a867172f682baf3c6c7da4fd06d70a
Pablo Perdigão <pabloagps@gmail.com>
2024-05-25 16:04:46 -0300

    adicionando e removendo a palavra "open" para troca da mensagem, adicionando a funcioalidade ao botão para ser fechado script.js

--- script.js
+++ script.js
@@ -6,41 +6,46 @@
 
   const modalWrapper = document.querySelector('.modal-wrapper')
   const modalMessage = document.querySelector('.modal .title span')
-  const modalbtnClose = document.querySelector('.modal button.close')
+  const modalBtnClose = document.querySelector('.modal button.close')
 
-// abrir o modal
-  modalMessage.innertext = "anything"
-// add "open" para trocar a mensagem do modal
-  modalWrapper.classList.add('open')
-
-/* 3 maneiras de criar e atribuir função a um evento */
   
-/*1ª forma:
-    form.onsubmit = function() {}
+  
+  /* 3 maneiras de criar e atribuir função a um evento */
+  
+  /*1ª forma:
+   form.onsubmit = function() {}
   */
-
+ 
   /* 2ª forma:arrow function
     form.onsubmit = () => {}
   */
-
+  
   /* 3ª forma:
     form.onsubmit = handlesubmit
     function handleSubmit() {
-
+      
     }
   */
-
-form.onsubmit = event => {
+ 
+ form.onsubmit = event => {
   event.preventDefault()
+   
+   const weight = inputWeight.value
+   const height = inputHeight.value
+   
+   const result = IMC(weight, height)
+   const message = `Seu IMC é de ${result}`
+   
+   // abrir o modal
+     modalMessage.innerText = message
 
-  const weight = inputWeight.value
-  const height = inputHeight.value
-
-  console.log(weight, height)
+   // add "open" para trocar a mensagem do modal
+      modalWrapper.classList.add('open')
 }
 
-
-
+// remove "open" para trocar a mensagem do modal
+  modalBtnClose.onclick = () => 
+    modalWrapper.classList.remove('open')
 
 // função do IMC
   function IMC(weight, height) {
