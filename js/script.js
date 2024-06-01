5042cbe51a9bd6d66b12dbf1559ee109f4200096
Pablo Perdigão <pabloagps@gmail.com>
2024-06-01 11:09:38 -0300

    Create folder js and add files js on this folder

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// import modal.js
+  import { Modal } from './modal.js'
+
+// variáveis - variables
+
+  const form = document.querySelector('form')
+  const inputWeight = document.querySelector('#weight')
+  const inputHeight = document.querySelector('#height')
+
+
+ form.onsubmit = event => {
+  event.preventDefault()
+   
+   const weight = inputWeight.value
+   const height = inputHeight.value
+   
+   const result = IMC(weight, height)
+   const message = `Seu IMC é de ${result}`
+   
+   // abrir o modal
+     Modal.message.innerText = message
+     Modal.open()
+  }
+    
+
+// função do IMC
+  function IMC(weight, height) {
+    return (weight / ((height / 100) ** 2)).toFixed(2)
+  }
