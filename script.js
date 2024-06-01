3103fbd556df75ef81f818265cadb78d801fa907
Pablo Perdigão <pabloagps@gmail.com>
2024-06-01 11:06:09 -0300

    import modal

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
+// import modal.js
+  import { Modal } from './modal.js'
+
 // variáveis - variables
 
   const form = document.querySelector('form')
   const inputWeight = document.querySelector('#weight')
   const inputHeight = document.querySelector('#height')
 
-// object literal
-  const Modal = {
-
-    wrapper: document.querySelector('.modal-wrapper'),
-    message: document.querySelector('.modal .title span'),
-    buttonClose: document.querySelector('.modal button.close'),
-
-  // shorthand funtion  
-    open() {
-      Modal.wrapper.classList.add('open')
-    },
-    close() {
-      Modal.wrapper.classList.remove('open')
-    }
-  }
-
-  Modal.close()
 
  form.onsubmit = event => {
   event.preventDefault()
@@ -34,12 +20,8 @@
    // abrir o modal
      Modal.message.innerText = message
      Modal.open()
-    }
+  }
     
-    // fechar o modal
-      Modal.buttonClose.onclick = () => 
-  
-      Modal.close()
 
 // função do IMC
   function IMC(weight, height) {
